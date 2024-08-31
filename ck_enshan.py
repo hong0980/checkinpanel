10c795a387725b7a8dd8e081cdd8064f3bb49adb
hong0980 <hong0980@qq.com>
2024-09-01 00:48:14 +0800

    update@2024-09-01-00-48

--- ck_enshan.py
+++ ck_enshan.py
@@ -4,9 +4,10 @@ cron: 25 3 0,12 * * *
 new Env('恩山论坛 签到');
 """
 
+import re, datetime
 from utils import get_data
 from notify_mtr import send
-import re, requests, datetime
+from requests_html import HTMLSession
 
 class Enshan:
     def __init__(self, check_items):
@@ -14,34 +15,30 @@ class Enshan:
 
     @staticmethod
     def sign(cookie, i):
-        res, session = '', requests.session()
-        headers = {
-            "Cookie": cookie,
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/102.0.0.0 Safari/537.36",
-        }
+        url, headers, session, current_date = 'https://www.right.com.cn/forum/home.php', \
+        {"Cookie": cookie}, HTMLSession(), datetime.datetime.now().strftime("%Y-%m-%d")
         try:
-            r = session.get("https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit&showcredit=1", headers=headers)
+            params = {'mod': 'spacecp', 'ac': 'credit', 'showcredit': '1'}
+            r = session.get(url, headers=headers, params=params)
             name = re.findall(r'访问我的空间">(.*?)</a>', r.text)
             name = name[0] if name else None
             if name is None:
                 return (f"<b><span style='color: red'>签到失败</span></b>\n"
                         f"账号({i})无法登录！可能Cookie失效，请重新修改")
 
-            r = session.get('https://www.right.com.cn/forum/home.php?mod=spacecp&ac=credit', headers=headers)
-            today = re.findall(rf'<td>{datetime.datetime.now().strftime("%Y-%m-%d")}.*?</td>', r.text)
-            sign_msg = f"<b><span style='color: red'>签到失败</span></b>" \
-                       if not today else "<b><span style='color: green'>签到成功</span></b>"
-            pattern = r'(恩山币|积分|贡献):\s*</em>(\d+)'
-            matches = re.findall(pattern, r.text)
-            res = f"---- {name} 恩山论坛 签到结果 ----\n{sign_msg}\n\n<b>账户信息</b>\n"
-            res += ''.join([f'{match[0]}: {match[1]}\n' for match in matches])
+            params = {'mod': 'spacecp', 'ac': 'credit'}
+            r = session.get(url, headers=headers, params=params)
+            today = re.search(rf'<td>{current_date}.*?</td>', r.text)
+            color, status = ('green', '成功') if today else ('red', '失败')
+            matches = re.findall(r'(恩山币|积分|贡献):\s*</em>(\d+)', r.text)
+            points = ''.join([f'{match[0]}: {match[1]}\n' for match in matches])
+            return (f'---- {name} 恩山论坛 签到结果 ----\n'
+                    f"<b><span style='color: {color}'>签到{status}</span></b>\n\n"
+                    f'<b>账户信息</b>\n{points}')
 
         except Exception:
             import traceback
             return f"<b><span style='color: red'>未知异常：</span></b>\n{traceback.format_exc()}"
-        return res
 
     def main(self):
         msg_all = ""
--- ck_nasyun.py
+++ ck_nasyun.py
@@ -1,38 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-cron: 55 0 0 * * *
+cron: 55 2 0 * * *
 new Env('那是云 签到');
 """
-import re, requests
+
+import re, datetime
 from utils import get_data
 from notify_mtr import send
+from requests_html import HTMLSession
 
-class nasyun:
+class NASYUN:
     def __init__(self, check_items):
         self.check_items = check_items
 
     @staticmethod
     def sign(cookie, i):
-        session = requests.session()
-        url = 'http://www.nasyun.com/home.php?mod=spacecp&ac=credit&op=base'
-        headers = {
-            "Cookie": cookie,
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/102.0.0.0 Safari/537.36",
-        }
+        url, headers = 'http://www.nasyun.com/home.php', {"Cookie": cookie}
+        s, current_date = HTMLSession(), datetime.datetime.now().strftime("%Y-%m-%d")
         try:
-            r = session.get('http://www.nasyun.com/home.php', headers=headers)
+            r = s.get(url, headers=headers)
             name = re.findall(r'title="访问我的空间" >(\w+)</a></div>', r.text)
             name = name[0] if name else None
             if name is None:
                 return (f"<b><span style='color: red'>签到失败</span></b>\n"
                         f"账号({i})无法登录！可能Cookie失效，请重新修改")
 
-            r = session.get(url, headers=headers)
-            pattern = r'<em>\s*(云币|贡献|活跃|积分):\s*</em>(\d+)'
-            matches = re.findall(pattern, r.text)
-            res = f"---- {name} 那是云 签到结果 ----\n\n<b>账户信息</b>\n"
+            params = {'mod': 'spacecp', 'ac': 'credit', 'op': 'base'}
+            f = s.get(url, headers=headers, params=params)
+
+            params.update({'op': 'log', 'suboperation': 'creditrulelog'})
+            p = s.get(url, headers=headers, params=params)
+
+            today = re.findall(rf'每天登录.*?(\d+)</td>.*?{current_date}', p.text, re.DOTALL)
+            msg = f"<b><span style='color: green'>签到成功</span></b>\n累计登录：{today[0]} 次\n" \
+                  if today else "<b><span style='color: red'>签到失败</span></b>\n"
+
+            matches = re.findall(r'(云币|贡献|活跃|积分):\s*</em>(\d+)', f.text)
+            res = f"---- {name} 那是云 签到结果 ----\n{msg}\n<b>账户信息</b>\n"
             res += ''.join([f'{match[0]}: {match[1]}\n' for match in matches])
 
         except Exception:
@@ -41,15 +45,13 @@ class nasyun:
         return res
 
     def main(self):
-        msg_all = ""
+        msg_all = ''
         for i, check_item in enumerate(self.check_items, start=1):
             cookie = str(check_item.get("cookie"))
             msg_all += f'{self.sign(cookie, i)}\n\n'
         return msg_all
 
 if __name__ == "__main__":
-    _data = get_data()
-    _check_items = _data.get("NASYUN", [])
-    result = nasyun(check_items=_check_items).main()
+    result = NASYUN(check_items=get_data().get("NASYUN", [])).main()
     send("那是云 签到", result)
     # print(result)
